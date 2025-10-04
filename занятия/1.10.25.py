94be589c93a186e1e4b26a6c9171e91614d5b8d2
Maksim <unitf40@gmail.com>
2025-10-04 19:41:03 +0200

    1

---
+++ занятия/1.10.25.py
@@ -0,0 +1,75 @@
+#Переменная - именновая ячейка в памяти
+test = 10
+user_name="Alice"#строковый тип - string (str)
+max_value=100#численный тип - intereg (int)
+our_float= 1.6#число с плавающей точкой (float)
+logical=True# True/False (1 или 0) - boolean (bool)
+
+# id - возвращает адрес в памяти
+print(id(user_name), max_value, our_float, logical)
+a=2
+b=2345634
+a=b
+print(id(a) ,id(b) )
+
+#определяет тип переменной (объекта)
+a=123
+print(type(a))
+
+a="1"
+a=int(a)
+print(type(a))
+a=int(1)#переводит в int
+print(type(a), a)
+a=float(a)#переводит в float
+print(type(a), a)
+a=str(a)#переводит в str
+print(type(a), a)
+a=bool(a)#переводит в bool (логический тип)
+print(type(a), a)
+
+
+#задача 2
+#age=16
+#print('Мне',age,'лет')
+#индекс начинается с 0
+#index всегда целое числоо
+a="Текст"
+#a- имя, a[index: int]
+#print(a[1]) #синтаксис получения элемента по индексу
+
+my_list=[1,2,3,4,5,"Привет"] #тип данных (list)
+print(my_list[3].__sizeof__()) #показывает размер в битах
+
+my_list=[1,2,3,4,5,6,7,8,9,10] #always list (нет массивов)
+# * / + - (базовые операторы)
+
+print(my_list[1]/3-2)
+
+#{}- dict (словарь) в качестве аргументов ключ: значение
+dictonary={"a": 1000}
+print(dictonary["a"])
+
+#input - команда ввода данных
+a=input('Введите текст: ')
+print("Вы ввели:", a)
+
+a=int(input())
+b=int(input())
+print(a+b)
+
+#кортеж- набор неизменяемых данных (tuple)
+a=(1,2,3,4,5,6,6)
+print(a, type(a))
+
+#set- набор упорядоченных по возрастанию уникальных значений (неизменяемый)
+a={3,2,1,0,-1,-1,-2}
+print(a, type(a))
+
+list_1=[1,2,3,4,5,6,6,7,7,8,8,9,0]
+print(set(list_1))
+
+a=["Раз","Два","Три "]
+print(a[0],'-',a[1],'-',a[2],sep='')
+#аргумент set
+print(a[0],a[1],a[2],sep='-')
---
+++ занятия/4.10.25.py
@@ -0,0 +1,42 @@
+#1
+student = {"Имя": "Иван","Фамилия": "Иванов", "Возраст": 17,"Класс": "11A"}
+print(student)
+#2
+a=45
+a=str(a)
+print('Число в строке:',a)
+#3
+a="sdfsdfs"
+b="sdfsdf"
+c="sfsdfdsf"
+print(a,b,c,sep=",")
+#6
+#a=(input())
+#b=(input())
+#a=float(a)
+#b=float(b)
+#a=int(a)
+#b=int(b)
+#print(a+b)
+#7
+#a,b,c=input(),input(),input()
+#print(a,b,c,sep=':')
+
+#split-разделитель
+#a=input().split(" ")
+#print(a[0],a[1],a[2],sep=':')
+
+#8
+#text="Python"
+#text=list(text)
+#print(text)
+#9
+#my_tuple=(1,2,3,4)
+#my_list=list(my_tuple)
+#print(id(my_tuple),id(my_list),sep=';')
+
+a=(1,2,3,4)
+b=[1,2,3,4]
+b[0]=5
+print(a)
+print(b)
