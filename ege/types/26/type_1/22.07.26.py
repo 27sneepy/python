03ba7ac704a215b7c2f9a1852b64ae1a28b97e14
Maksim <161756583+27sneepy@users.noreply.github.com>
2026-07-22 20:39:46 +0300

    \\

---
+++ ege/types/26/type_1/22.07.26.py
@@ -0,0 +1,33 @@
+# 2601
+# data=[50,28,32,40]
+# with open("../files/2601.txt") as f:
+#     data=list(map(int,f))
+# data=sorted(data,reverse=True)
+#
+# res=[data[0]]
+#
+# for i in data[1:]:
+#     if res[-1]-i>=9:
+#         res.append(i)
+# print(len(res),res[-1])
+
+
+# 2607
+# with open("../files/2607.txt") as f:
+#     data=sorted(list(map(int,f)),reverse=True)
+# res=[data[0]]
+# for i in data[1:]:
+#     if res[-1]-i>=4:
+#         res.append(i)
+# print(len(res),res[-1])
+
+# 2613
+# with open("../files/2613.txt") as f:
+#     f.readline()
+#     data=sorted(list(map(int,f)),reverse=True)
+# res=[data[0]]
+# for i in data[1:]:
+#     if res[-1]-i>=7:
+#         res.append(i)
+# print(len(res),res[-1])
+
