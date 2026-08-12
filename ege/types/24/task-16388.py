e9ffdb822fcf734ffce6f646d2cc6eeb82ce4761
Maksim <161756583+27sneepy@users.noreply.github.com>
2026-08-12 20:11:01 +0200

    hw

---
+++ ege/types/24/task-16388.py
@@ -0,0 +1,17 @@
+from re import finditer
+
+with open("../../../files/24_16388.txt") as f:
+    data=f.readline()
+
+# KLMN
+# группа может быть неполной
+# посл сод не менее 1 полной группы
+# MNKLMNKLMNK
+
+# data="MNKLMNKLMNK"
+
+pattern = r"[LMN|MN|N](KLMN)+[KLM|KL|K]"
+
+matches=[match.group() for match in finditer(pattern,data)]
+
+print(len(max(matches, key = len)))
---
+++ ege/types/24/task-17641.py
@@ -0,0 +1,14 @@
+from re import finditer
+
+with open("../../../files/24_17641.txt") as f:
+    data=f.readline()
+
+# 9203948*0+92384*0x
+# 98*0*
+number = r"(0|[1-9][0-9]*)"
+zero = rf"({number}\*)*0(\*{number})*"
+pattern = rf"{zero}(\+{zero})*"
+
+matches=[match.group() for match in finditer(pattern,data)]
+
+print(len(max(matches, key = len)))
